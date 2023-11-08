f8ed5401b5b65bf355555894b8caf10447024add
Rayhan Hassou <Rayhan.HASSOU@etu.uca.fr>
2023-11-08 11:50:56 +0100

    the desing for the button is finished

--- LeftOvers/App.js
+++ LeftOvers/App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import CustomButton from './components/CustomButton';
 
 export default function App() {
 
+  const handleButtonPress = () => {
+    console.log("test 1");
+  };
+
+  const handleButtonPress2 = () => {
+    console.log("test 2");
+  };
 
   return (
     <View style={styles.container}>
       <Text>Ceci va etre la page qui charge le splashScreen</Text>
       <StatusBar style="auto" />
+      <CustomButton title="Appuyez-moi" onPress={handleButtonPress} />
+      <CustomButton title="Autre test" onPress={handleButtonPress2} />
     </View>
   );
 }
