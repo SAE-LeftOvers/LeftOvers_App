d0ace4f2558292b5730787067728d8d03e22af13
Remi REGNAULT <Remi.REGNAULT@etu.uca.fr>
2023-11-22 10:49:54 +0100

    feat add style to bottom bar

--- LeftOvers/navigation/CookingStackScreen.tsx
+++ LeftOvers/navigation/CookingStackScreen.tsx
@@ -3,19 +3,29 @@ import { StyleSheet } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import IngredientSelection from '../screens/IngredientSelection';
+import { HeaderTitle } from './Utils';
 
 const CookingStack = createNativeStackNavigator()
 
 export default function CookingStackScreen() {
   return (
-    <CookingStack.Navigator>
-      <CookingStack.Screen
-        name='IngredientSelection'
-        component={IngredientSelection}
-      />
-    </CookingStack.Navigator>
+      <CookingStack.Navigator>
+          <CookingStack.Screen
+              name='IngredientSelection'
+              component={IngredientSelection}
+              options={{
+                  headerStyle: styles.headerBarContainer,
+                  headerTitle: () => (
+                      <HeaderTitle title='Profile Modification'/>
+                  )
+              }}
+          />
+      </CookingStack.Navigator>
   )
 }
 
 const styles = StyleSheet.create({
+    headerBarContainer: {
+        backgroundColor: '#F2F0E4',
+    },
 })
