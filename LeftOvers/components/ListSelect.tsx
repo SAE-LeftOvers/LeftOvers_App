39484c90e9e1cee8335b8f92f93ecd5790b557f7
Remi REGNAULT <Remi.REGNAULT@etu.uca.fr>
2023-11-29 08:55:15 +0100

    removing unused variables and imports

--- LeftOvers/components/ListSelect.tsx
+++ LeftOvers/components/ListSelect.tsx
@@ -1,8 +1,6 @@
 import React, { useContext } from 'react';
-import {StyleSheet, Text, TextInput, View, Image, FlatList} from 'react-native';
-import ValidateButton from './ValidateButton';
-import HeaderFlatList from './HeaderFlatList';
-import { MultipleSelectList, SelectList } from 'react-native-dropdown-select-list'
+import { StyleSheet, Image } from 'react-native';
+import { MultipleSelectList } from 'react-native-dropdown-select-list'
 import ColorContext from '../theme/ColorContext';
 
 type ListProps = {
@@ -12,7 +10,7 @@ type ListProps = {
 
 export default function ListSelect(props: ListProps) {
     const [selected, setSelected] = React.useState([]);
-    const { colors, toggleColors } = useContext(ColorContext);
+    const colors = useContext(ColorContext).colors;
 
     const styles = StyleSheet.create({
         titleBar: {
